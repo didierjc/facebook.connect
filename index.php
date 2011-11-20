026a5757a639368ce0af7db07767c4c83888cb56
Didier Jean Charles <didijc@gmail.com>
2011-11-20 09:27:18 -0500

    this round of revisions includes the following: friendly URLs, modification to config->main.php and db credentials added

--- index.php
+++ index.php
@@ -4,10 +4,28 @@
 $yii=dirname(__FILE__).'/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
-// remove the following lines when in production mode
+// remove / comment the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-
+// Include Yii bootstrap file
 require_once($yii);
 Yii::createWebApplication($config)->run();
+
+
+/*
+ * High level Application Lifecycle
+ * 1- Pre-initialize: Capplication::preinit()
+ * 2- Setup class auto loader & error handling
+ * 3- Register core app components
+ * 4- Load app config
+ * 5- Initialize App: Capplication::init()
+ *      a- Register app behaviors
+ *      b- Load static app components
+ * 6- Raise onBeginRequest event
+ * 7- Process user request
+ *      a- Resolve user request
+ *      b- Create controller
+ *      c- Run controller
+ * 8- Raise onEndRequest event
+ */
--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Facebook.Connect',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -20,14 +20,12 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'ny031c7d',
 		 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
 	),
 
 	// application components
@@ -36,30 +34,46 @@ return array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
 		),
-		// uncomment the following to enable URLs in path-format
 		/*
+		 * Complete URL management for a Web application involves two aspects.
+		 *
+		 * First, when a user request comes in terms of a URL, the application needs to parse it into
+		 * understandable parameters.
+		 *
+		 * Second, the application needs to provide a way of creating URLs so that the created URLs can be
+		 * understood by the application. For a Yii application, these are accomplished with the help of
+		 * CUrlManager.
+		 */
+		// uncomment the following to enable URLs in path-format
 		'urlManager'=>array(
 			'urlFormat'=>'path',
+			/*
+			 * To remove index.php from the URLs you need to do two things:
+			 *	1) add another parameter: 'showScriptName' => false, this will remove index.php from the
+			 *		links
+			 *	2) In the webroot folder of your app, add a .htaccess rule
+			 *		(for Apache, IIS will need different settings) with a rewrite rule of:
+			 *			RewriteRule . index.php
+			 *		This will help your web server still call index.php
+			 */
+			'showScriptName'=>false,
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
-		'db'=>array(
+		/*'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
+		),*/
 		// uncomment the following to use a MySQL database
-		/*
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=facebook',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => 'ny031c7d',
 			'charset' => 'utf8',
 		),
-		*/
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
             'errorAction'=>'site/error',
@@ -85,6 +99,6 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
+		'adminEmail'=>'didijc@gmail.com',
 	),
 );
